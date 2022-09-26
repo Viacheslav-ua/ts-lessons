ed0a5867fee71a487683655bd986f7a67d3c1017
Viacheslav-ua <atph@ukr.net>
2022-09-26 19:30:17 +0300

    fix

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 "use strict";
-var root = document.getElementById('root');
-var Cat = /** @class */ (function () {
-    function Cat(name) {
-        this.name = name;
-    }
-    return Cat;
-}());
-var kitty = new Cat('Kitty');
-if (root) {
-    root.innerHTML = "My Cat is <b>".concat(kitty.name, "</b>");
-}
