fbe75016354107af0aef9fffe3935d4a3438849c
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2022-09-26 06:23:43 +0300

    lesson-#5

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 "use strict";
-var root = document.getElementById('root');
-var Cat = /** @class */ (function () {
-    function Cat(name) {
-        this.name = name;
+function greatSum(a) {
+    var rest = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        rest[_i - 1] = arguments[_i];
     }
-    return Cat;
-}());
-var kitty = new Cat('Kitty');
-if (root) {
-    root.innerHTML = "My Cat is <b>".concat(kitty.name, "</b>");
+    if (!rest.length) {
+        return a;
+    }
+    return rest.reduce(function (acc, item) { return acc + item; }, a);
 }
+function overLoadedSum(a) {
+    var rest = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        rest[_i - 1] = arguments[_i];
+    }
+    if (!rest.length) {
+        return a;
+    }
+    return rest.reduce(function (acc, item) { return acc + item; }, a);
+}
+console.log(overLoadedSum(23, 3, 45, 254, 33));
+console.log(overLoadedSum('sd', '3', '45', '254', '33'));
+// Callback as param
+function sumWithCallback(callback, a) {
+    var rest = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        rest[_i - 2] = arguments[_i];
+    }
+    if (!rest.length) {
+        return a;
+    }
+    return rest.reduce(callback);
+}
+console.log('Callback', sumWithCallback(function (ac, item) { return ac + item; }, 4, 5, 4, 3));
--- index.ts
+++ index.ts
@@ -1,15 +1,38 @@
-const root = document.getElementById('root')
-
-class Cat {
-  name
-  constructor(name: string) {
-    this.name = name
+function greatSum(a: number, ...rest: number[]): number {
+  if(!rest.length) {
+    return a
   }
+
+  return rest.reduce((acc, item) => acc + item, a)
 }
 
-const kitty = new Cat('Kitty')
+// Overload
 
-if (root) {
-  root.innerHTML = `My Cat is <b>${kitty.name}</b>`
+function overLoadedSum(a: string, ...rest: string[]): string 
+function overLoadedSum(a: number, ...rest: number[]): number
+function overLoadedSum(a: any, ...rest: Array<any>): any {
+  if(!rest.length) {
+    return a
+  }
+
+  return rest.reduce((acc, item) => acc + item, a)
 }
+
+console.log(overLoadedSum(23, 3, 45, 254, 33))
+console.log(overLoadedSum('sd', '3', '45', '254', '33'))
+
+// Callback as param
+
+function sumWithCallback(
+  callback: (ac: number, item: number) => number,
+  a: number,
+  ...rest: number[]
+): number {
+  if(!rest.length) {
+    return a
+  }
+
+  return rest.reduce(callback)
+}
+console.log('Callback', sumWithCallback((ac, item) => ac + item, 4, 5, 4, 3));
 
